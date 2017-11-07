7b12edb11abc96dceaf80aeba9fa75fc0a68b088
Kaeladin <trix1011@gmail.com>
2017-11-07 02:44:56 -0600

    laser center finder working for first test case

--- laserlocator.m
+++ laserlocator.m
@@ -1,42 +1,45 @@
 laser=imread('lasers.png');
-center=chromacenter(laser);
+[center,redness]=chromacenter(laser);
 %laser2=w2b(laser);
 imshow(laser);
 hold on
-plot(center,'g*');
+plot(center(2),center(1),'g*');
  
 
-function[center]=chromacenter(image)
+function[center,redness]=chromacenter(image)
 center=[0,0];
 x=0;
 y=0;
 RED=0;
 bounds=size(image);
+localred=zeros(bounds);
+localred=redlevel(image);
+RED=sum(localred(:));
+
 for i=1:bounds(1)
     for j=1:bounds(2)
-        localred=redlevel(image,i,j);
-        RED=RED+localred;
-        x=x+localred*i;
-        y=y+localred*j;
+        x(i,j)=double(localred(i,j)).*i;
+        y(i,j)=double(localred(i,j)).*j;
     end
 end
-num=[x,y];
-center=num/RED;
+num=[sum(x(:)),sum(y(:))]
+center=num./RED
+redness=localred;
 end
 
 
-
-
-function[blackbackground]=w2b(image)
-white=[255,255,255];
-blackbackground=zeros(size(image));
-for i=1:size(image,1)
-    for j=1:size(image,2)
-        for k=1:3  
-         if(image(i,j,k)~=255)
-             blackbackground(i,j,k)=image(i,j,k)
-         end
-        end
-    end
-end
-end
+% 
+% 
+% function[blackbackground]=w2b(image)
+% white=[255,255,255];
+% blackbackground=zeros(size(image));
+% for i=1:size(image,1)
+%     for j=1:size(image,2)
+%         for k=1:3  
+%          if(image(i,j,k)~=255)
+%              blackbackground(i,j,k)=image(i,j,k)
+%          end
+%         end
+%     end
+% end
+% end
--- redlevel.m
+++ redlevel.m
@@ -1,6 +1,6 @@
-function[redness]=redlevel(image,x,y)
-red=image(x,y,1);
-blue=image(x,y,2);
-green=image(x,y,3);
-redness = max(0, red - (blue/2 + green/2));
+function[redness]=redlevel(image)
+red=image(:,:,1);
+blue=image(:,:,2);
+green=image(:,:,3);
+redness = (red - (blue/2 + green/2));
 end
