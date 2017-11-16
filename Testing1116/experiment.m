56682093b280979464686b812164daa962ee1e43
DavidMerzJr <dmmjr2@gmail.com>
2017-11-16 15:26:13 -0600

    Updates to Experimental Procedure
    Edits to experiment.m:
      - now reads in test images from 'Testing1116' folder
      - added automatic display of test images
      - renamed loop variables for readability
      - changed position of dialog boxes - may not appear on-screen if you have only a single monitor

--- Testing1116/experiment.m
+++ Testing1116/experiment.m
@@ -1,24 +1,28 @@
+clc;
+
 screen=["lcd","kindle"];
 trial=[1,2];
 lens=[3,1,2];
 isLens=["in path","out of the way"];
 period=[2,4];
-orientation=['v','h'];
+orientation=['V','H'];  % Changed to capitals to match case of test image names - DM
 
-
-
+figure;
+H = gcf;
 
 %%
-for a=1:2
-    dialogwait("Set screen to "+screen(a));
-   for b=1:2
-       for c=1:3
-           dialogwait("Trial "+trial(b)+" | "+"Insert lens "+lens(c));
-           for d=1:2
-                   dialogwait("Trial "+trial(b)+" | "+"Make sure lens is "+isLens(d));
-               for e=1:2
-                   for f=1:2
-                          dialogwait("Trial "+trial(b)+" | Take pic of "+"Period "+period(e)+", Orientation: "+orientation(f));
+for scr=1:1   % Changed to only lcd due to manual testing of kindle - DM
+    dialogwait("Set screen to "+screen(scr));
+   for trl=1:2
+       for lns=1:3
+           dialogwait("Trial "+trial(trl)+" | "+"Insert lens "+lens(lns));
+           for isLns=1:2
+                   dialogwait("Trial "+trial(trl)+" | "+"Make sure lens is "+isLens(isLns));
+               for prd=1:2
+                   for ornt=1:2
+                       imshow(strcat(num2str(period(prd)),'px',orientation(ornt),'.png'),'InitialMagnification',100);    % If unusual zoom things start happening, remove the last two imshow() arguments
+                       movegui(H,'center');
+                       dialogwait("Trial "+trial(trl)+" | Take pic of "+"Period "+period(prd)+", Orientation: "+orientation(ornt));
                    end
                end
            end
@@ -26,9 +30,11 @@ for a=1:2
    end
 end
 
+close(H);
+
 %%
 function dialogwait(name)
-    d = dialog('Position',[300 300 500 150],'Name',name);
+    d = dialog('Position',[-800 300 500 150],'Name',name);
 
     txt = uicontrol('Parent',d,...
                'Style','text',...
