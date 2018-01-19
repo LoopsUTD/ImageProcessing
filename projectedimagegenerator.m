803a386b928d19cb96eae434d095700c735ae6f4
DavidMerzJr <dmmjr2@gmail.com>
2018-01-19 14:37:36 -0600

    Changed image generation to square blobs
    for testing reasons

--- projectedimagegenerator.m
+++ projectedimagegenerator.m
@@ -5,14 +5,16 @@
 orientation="H";
 
 %%
-xsize=1280;
-ysize=720;
+xsize=960;
+ysize=540;
 image=zeros(ysize,xsize);
 %%
-period=10.0;                     % This is now how often a green pixel appears - 1 in [period]
+period=9.0;                     % This is now how often a red pixel appears - 1 in [period]
+% If possible, select a period of 3n+1, where n is an integer
+%step=floor(period/3);
 % dutycycle=0.5;                % used for alternating bars
 % cutoff=period*(1-dutycycle);  % used for alternating bars
-cutoff=1.0;
+cutoff=3.0;
 nImages=1;
 colorvect=[0,255,0];
 %%
@@ -23,24 +25,31 @@ for idx=1:nImages
     image=zeros(ysize,xsize);
     k=idx-1;
     for i=1:1:ysize  
-     k=k+1;
-         if k<=cutoff
-             m = 1;
-             for j=1:1:xsize
-                 m=m+1;
-                 if m<=cutoff
-                     image(i,j,1:3)=ones(1,1).*colorvect;
-                 end
-                 if m==period
-                     m=0;
-                 end
-             end
+        k=k+1;
+        if k<=cutoff
+            m = 0;
+            for j=1:1:xsize
+                m=m+1;
+                if m<=cutoff
+                    image(i,j,1:3)=[0,255,0];
+%                     image(i,j,1:3)=[255,0,0];
+%                     if j+floor(period/3)<=xsize
+%                         image(i,j+floor(period/3),1:3)=[0,255,0];
+%                         if j+floor(2*period/3)<=xsize
+%                             image(i,j+floor(2*period/3),1:3)=[0,0,255];
+%                         end
+%                     end
+                end
+                if m==period
+                    m=0;
+                end
+            end
           % image(i,:,1:3)=ones(xsize,1).*colorvect;  % from when this code
           % only made horizontal lines
-         end
-         if k==period
-             k=0;
-         end
+        end
+        if k==floor(period)
+            k=0;
+        end
     end
 
 imshow(image)
@@ -48,7 +57,7 @@ frame=getframe(h);
 im{idx}=frame2im(frame);
 end
 
-imwrite(image,'TestImages/10px.png');
+imwrite(image,'TestImages/039pxG.png');
 
 
 %%
